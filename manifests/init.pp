333ee74710325426bd08b4b9f82621ad068fb5c5
Les Aker <me@lesaker.org>
2026-04-06 20:15:53 -0400

    simplify config

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,32 +10,11 @@ class ecowitt (
 ) {
   $custom_file = "server {
     server_name ${hostname};
-    listen *:${port} http2;
-    listen [::]:${port} http2;
-
-    add_header X-XSS-Protection          \"1; mode=block\" always;
-    add_header X-Content-Type-Options    \"nosniff\" always;
-    add_header Referrer-Policy           \"no-referrer-when-downgrade\" always;
+    listen *:${port};
+    listen [::]:${port};
 
     location /webhook {
         proxy_pass                         ${target};
-        proxy_http_version                 1.1;
-        proxy_cache_bypass                 \$http_upgrade;
-
-        proxy_set_header Upgrade           \$http_upgrade;
-        proxy_set_header Connection        \$connection_upgrade;
-        proxy_set_header Host              \$host;
-        proxy_set_header X-Real-IP         \$remote_addr;
-        proxy_set_header Forwarded         \$proxy_add_forwarded;
-        proxy_set_header X-Forwarded-For   \$proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto \$scheme;
-        proxy_set_header X-Forwarded-Host  \$host;
-        proxy_set_header X-Forwarded-Port  \$server_port;
-
-        proxy_connect_timeout              60s;
-        proxy_send_timeout                 60s;
-        proxy_read_timeout                 60s;
-
     }
 }"
 
